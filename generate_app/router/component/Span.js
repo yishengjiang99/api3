326341a23dfafd5924b213ececc239a4c8f630dd
Yisheng Jiang <yisheng.jiang@gmail.com>
2020-08-07 20:15:22 -0700

    react-app-template

---
+++ generate_app/router/component/Span.js
@@ -0,0 +1,2 @@
+import React from 'react';
+export const Span = ( props )=><span>{props.children}</span>
---
+++ generate_app/router/webpack.config.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+  entry: './index.js',
+
+  output: {
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      }
+    ],
+  }
+}
