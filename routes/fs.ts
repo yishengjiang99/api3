c94ebd067dc6147a20625241ffcc99e1f0ee6c22
Your Name <you@example.com>
2020-08-12 09:53:38 +0000

    ..

--- routes/fs.ts
+++ routes/fs.ts
@@ -3,12 +3,16 @@ var router = express.Router();
 import * as fs from "fs";
 import { resolve } from "path";
 import * as linfs from "../src/linfs";
-import * as serveIndex from "serve-index";
-
-router.get("/", serveIndex(linfs.rootdir));
-
+router.get("/:path", (req, res) => {
+  const path = resolve(linfs.rootdir, req.params.path);
+  (fs.existsSync(path) === false && res.status(404) && res.end("404")) ||
+    (fs.statSync(path).isFile() && fs.createReadStream(path).pipe(res)) ||
+    fs.readdir(path, (err, output) => {
+      res.end(require("serve-index")(path).html);
+    });
+});
 router.get("/read/:path", (req, res) => {
-	const path = resolve(linfs.rootdir, req.params.path);
-	fs.createReadStream(path).pipe(res);
+  const path = resolve(linfs.rootdir, req.params.path);
+  fs.createReadStream(path).pipe(res);
 });
 module.exports = router;
