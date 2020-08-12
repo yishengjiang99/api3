7d933c59cc30a2bb7debeeacb05b77e1c221afc1
Yisheng Jiang <yisheng.jiang@gmail.com>
2020-08-12 03:22:22 -0700

    rm junk

--- https.ts
+++ https.ts
@@ -7,13 +7,13 @@ import * as db from "./src/db";
 import * as https from "https";
 import * as express from "express";
 import * as fs from "fs";
-import spotify from './routes/spotify';
-import yt from './routes/yt';
+import spotify from "./routes/spotify";
+import yt from "./routes/yt";
 
 const WebSocketServer = require("ws").Server;
 export const ssl = {
-  key: fs.readFileSync(process.env.PRIV_KEYFILE),
-  cert: fs.readFileSync(process.env.CERT_FILE),
+	key: fs.readFileSync(process.env.PRIV_KEYFILE),
+	cert: fs.readFileSync(process.env.CERT_FILE),
 };
 var options = ssl;
 
@@ -24,24 +24,20 @@ app.set("views", __dirname + "/views");
 app.set("view engine", "jsx");
 app.engine("jsx", require("./src/express-react-forked").createEngine());
 
-
 app.use("/spotify", require("./routes/spotify"));
 app.use("/yt", yt);
-
-
-// app.use("/(:vid).mp3", require("./routes/yt").ytmp3);
 
 app.use("/fs", require("./routes/fs"));
 app.use("/", express.static("../piano/build"));
 app.use("/views", express.static("./views"));
-app.use(function(req,res,next){
-  res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-  res.header('Expires', '-1');
-  res.header('Pragma', 'no-cache');
-  next();
-})
+app.use(function (req, res, next) {
+	res.header("Cache-Control", "private, no-cache, no-store, must-revalidate");
+	res.header("Expires", "-1");
+	res.header("Pragma", "no-cache");
+	next();
+});
 app.get("/api", (req, res) => {
-  res.render("welcome", { host: req.headers.host, t: "s" });
+	res.render("welcome", { host: req.headers.host, t: "s" });
 });
 
 app.get("/favicon.ico", require("./routes/favicon").favicon);
@@ -56,20 +52,20 @@ const { rtcHandler } = require("./lib/rtcsignal");
 rtcServer.on("connection", rtcHandler);
 
 httpsServer.on("upgrade", function upgrade(request, socket, head) {
-  const pathname = require("url").parse(request.url).pathname;
-  if (pathname.match(/signal/)) {
-    signalServer.wss.handleUpgrade(request, socket, head, function done(ws) {
-      const dbuser = db.getOrCreateUser(request.headers["set-cookie"]);
-      signalServer.requestContext[
-        request.headers["sec-websocket-key"]
-      ] = dbuser;
-      signalServer.wss.emit("connection", ws, request);
-    });
-  } else if (pathname.match(/rtc/)) {
-    rtcServer.handleUpgrade(request, socket, head, function done(ws) {
-      rtcServer.emit("connection", ws, request);
-    });
-  }
+	const pathname = require("url").parse(request.url).pathname;
+	if (pathname.match(/signal/)) {
+		signalServer.wss.handleUpgrade(request, socket, head, function done(ws) {
+			// // const dbuser = db.getOrCreateUser(request.headers["set-cookie"]);
+			// signalServer.requestContext[
+			//   request.headers["sec-websocket-key"]
+			// ] = dbuser;
+			signalServer.wss.emit("connection", ws, request);
+		});
+	} else if (pathname.match(/rtc/)) {
+		rtcServer.handleUpgrade(request, socket, head, function done(ws) {
+			rtcServer.emit("connection", ws, request);
+		});
+	}
 });
 
 httpsServer.listen(443); //process.argv[2] || 3000);
--- views/video.jsx
+++ views/video.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import SearchBar from './searchBar';
+import React, { useState } from 'react';
 const videopage = ({ videos }) =>
   <div>
-    <searchBar></searchBar>
+    <form action='/yt' method='post'>
+      <input name='query' type='text'></input>
+      <inpput type='submit' value='search'></inpput>
+    </form>
     {videos.map(v =>
       <span>
         <h3>{v.title}</h3>
         <img src={`https://i.ytimg.com/vi/${v.vid}/default.jpg`}></img>
-        <audio controls src={`https://www.grepawk.com/yt/${v.vid}.mp3`}></audio>
+        <audio controls src={`/yt/${v.vid}.mp3`}></audio>
         <div>{v.description}</div>
       </span>)}
   </div>
