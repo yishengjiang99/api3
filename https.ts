b3d87123a9bb39df033e04ae946f32f2dd8fe099
Your Name <you@example.com>
2020-08-26 03:23:54 +0000

    fix

--- https.ts
+++ https.ts
@@ -2,22 +2,28 @@
 
 import { resolve } from "path";
 import { execSync } from "child_process";
-import { readFile, createReadStream, exists } from "fs";
+import { readFile, readFileSync, existsSync, createReadStream, exists } from "fs";
 import * as db from "./src/db";
 import * as https from "https";
 import * as express from "express";
 import * as fs from "fs";
-import spotify from "./routes/spotify";
+import * as spotify from "./routes/spotify";
+import * as auth from './routes/auth'
 import yt from "./routes/yt";
 
-const WebSocketServer = require("ws").Server;
-export const ssl = {
-  key: fs.readFileSync(process.env.PRIV_KEYFILE),
-  cert: fs.readFileSync(process.env.CERT_FILE),
-};
-var options = ssl;
+const connect = require('connect')
+const app = express();
+const dspServer = connect();
+const apiServer = connect();
+var vhost = require("vhost");
+dspServer.use("/api/spotify", auth);
+dspServer.use("/", express.static("../grepaudio"));
 
-var app = express();
+app.use(vhost("piano.grepawk.com", express.static("../piano/build")));
+app.use(vhost("dsp.grepawk.com", dspServer));
+app.use(vhost("api.grepawk.com", apiServer));
+
+
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 app.set("views", __dirname + "/views");
@@ -26,24 +32,31 @@ app.engine("jsx", require("./src/express-react-forked").createEngine());
 
 app.use("/spotify", require("./routes/spotify"));
 app.use("/yt", yt);
-
+app.use("/auth", auth);
 app.use("/fs", require("./routes/fs"));
 app.use("/", express.static("../piano/build"));
 app.use("/views", express.static("./views"));
-// app.use(function (req, res, next) {
-//   res.header("Cache-Control", "private, no-cache, no-store, must-revalidate");
-//   res.header("Expires", "-1");
-//   res.header("Pragma", "no-cache");
-//   next();
-// });
-app.get("/api", (req, res) => {
-  res.render("welcome", { host: req.headers.host, t: "s" });
-});
+
+export const httpsTLS = {
+  key: readFileSync(process.env.PRIV_KEYFILE),
+  cert: readFileSync(process.env.CERT_FILE),
+  SNICallback: function (domain, cb) {
+    if (!existsSync(`/etc/letsencrypt/live/${domain}`)) {
+      cb();
+    }
+    cb(null, require('tls').createSecureContext({
+      key: readFileSync(`/etc/letsencrypt/live/${domain}/privkey.pem`),
+      cert: readFileSync(`/etc/letsencrypt/live/${domain}/fullchain.pem`),
+    }));
+  },
+};
+app.get("/auth", app.use(auth));
 
 app.get("/favicon.ico", require("./routes/favicon").favicon);
 app.use("/app", express.static(__dirname + "/static"));
+const WebSocketServer = require("ws").Server;
 
-const httpsServer = https.createServer(options, app);
+const httpsServer = https.createServer(httpsTLS, app);
 
 const signalServer = new (require("./src/signal").Server)();
 const rtcServer = new WebSocketServer({ noServer: true });
@@ -67,11 +80,7 @@ httpsServer.on("upgrade", function upgrade(request, socket, head) {
     });
   }
 });
+const port = process.argv[2] || 443;
+httpsServer.listen(port); //process.argv[2] || 3000);
+console.log("listening on " + port); //
 
-httpsServer.listen(443); //process.argv[2] || 3000);
-console.log("listening on 443"); //
-// res.writeHead(200, "one moemnt", {
-//   "Content-Type": "image/jpeg",
-//   "set-cookie": "username=" + username,
-// });
-// r
