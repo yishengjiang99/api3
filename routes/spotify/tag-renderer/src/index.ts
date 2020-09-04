29499f04196b014f73ff1104f5d6574337314bda
Yisheng Jiang <yisheng.jiang@gmail.com>
2020-09-04 13:45:37 -0700

    clean

--- routes/spotify/tag-renderer/src/index.ts
+++
@@ -1,9 +0,0 @@
-export { TagRenderer } from "./tag-renderer";
-export {
-  TagTemplate,
-  createTemplateTags,
-  createTemplateTagsFromArray,
-  RegisterTokenIds,
-  Token,
-  TokenInvoke
-} from "./tag-template";
--- routes/spotify/tag-renderer/src/symbols.ts
+++
@@ -1,1 +0,0 @@
-export const TAG_TYPE = Symbol("tag-type");
