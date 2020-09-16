d570c0faae90edf4f1a60874224f64993f4e2067
Your Name <you@example.com>
2020-09-16 07:56:30 +0000

    waste of time forced chunking is  not a feature its a bug

--- src/stdin.ts
+++ src/stdin.ts
@@ -1,45 +1,48 @@
 /*eslint-disable no-console*/
-import * as azfs from "./azfs";
+import { BlobService, createBlobService, ErrorOrResult } from "azure-storage";
 import * as url from "url";
-import { runInThisContext } from "vm";
+
+const blobService: BlobService = createBlobService();
 
 export const stdinHandler = async (ws, request) => {
+  debugger;
   let exitCode = 1;
+  console.log("on connection  ")
   const xpath = url.parse(request.url).pathname.replace("/stdin/", "");
   const parts = xpath.split("/");
 
-  const container = parts[0] ? parts[0] : "public_file";
-  const blobname = parts[1] ? parts[1] : "new_file.txt";
-  const fh = await azfs.fopenw(xpath);
+  const container = "sounds";
+  const blobname = parts.join("-");
+
+  const fh = blobService.createWriteStreamToBlockBlob(container, blobname, (err, result, response) => {
+    if (err) ws.write("err:  " + err.message);
+    ws.write("writing to ");
+    ws.write(blobService.getUrl(container, blobname));
+  });
 
   fh.on("error", (err) => {
     ws.send("ERR:" + err.message);
   });
-
   ws.on("message", (message) => {
-    if (message.trim() === "commit")
-    {
-      azfs.blobClient.createBlobSnapshot(container, blobname, (err, resul) => {
-        if (err) ws.send(err.message);
-        else ws.send(JSON.stringify(resul));
-      });
-    } else if (message.trim() === "url")
-    {
-      const url = azfs.blobClient.getUrl(container, blobname);
+
+    if (message.trim() === "url") {
+      const url = blobService.getUrl(container, blobname);
       ws.send(url);
-    } else if (message.trim() === "EOF")
-    {
+    } else if (message.trim() === "EOF") {
       fh.end();
       ws.send("writing to " + xpath);
       ws.close();
-    } else
-    {
+    } else {
       fh.write(message);
-      ws.send(">" + message);
+      ws.send("> ");
     }
   });
 
   ws.on("close", () => {
     console.log("closed on code " + exitCode);
   });
+
+  console.log(parts, container);
+
+
 };
