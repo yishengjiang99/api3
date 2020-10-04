a4a1e558be52f244348b45f94659bdca2800fccf
Your Name <you@example.com>
2020-10-04 09:37:24 +0000

    wtf

--- src/db.ts
+++ src/db.ts
@@ -1,73 +1,60 @@
-import {
-	createPool,
-	Connection,
-	createConnection,
-	execute,
-} from "mysql2/promise";
 import Axios, { AxiosResponse } from "axios";
-import { response } from "express";
-// let pool = createPool({
-//   host: "localhost",
-//   user: process.env.db_user,
-//   password: process.env.db_password,
-//   database: "grepawk",
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-// });
 
-async function conn(): Connection {
-	return await createConnection({
-		host: "localhost",
-		user: process.env.db_user,
-		password: process.env.db_password,
-		database: "grepawk",
-		port: 3306,
-	});
+export async function dbQuery(sql: string, args = []) {
+	while (args) sql = sql.replace(" ? ", args[0]) && args.shift();
+	const html = require("child_process").execSync(`mysql -u ${process.env.db_user} -p${process.env.db_password} -e '${sql}' grepawk --html`).toString();
+	let idx = 0;
+
+	const strmatchall = (str: any, regex: RegExp) => {
+		const matches = [];
+		idx = 0;
+		while (idx < html.length)
+		{
+			var m = html.substring(idx).match(/<TH\>(.*?)\<\/TH>/);
+			if (!m) break;
+			matches.push(m);
+			idx = m['index'];
+		}
+		return matches;
+	}
+
+	const cols = strmatchall(html, /<TH\>(.*?)\<\/TH>/);
+	const rows = strmatchall(html, /<TD\>(.*?)\<\/TD>/);
+	return rows.reduce((jsonlist, idx, val) => {
+		if (idx % cols.length === 0) jsonlist.push({});
+		jsonlist[idx / cols.length][cols[idx % cols.length]] = val;
+	}, []);
+}
+export async function dbRow(sql: string, args = []) {
+	return dbQuery(sql, args);
 }
 
-export async function dbQuery(sql, args = []) {
-	const c = await conn();
-	const [results, fields] = await c
-		.query(sql, args)
-		.catch((e) => {
-			console.error(sql, args, e);
-		})
-		.finally(() => c.close());
-	//  c.close();
-	return results;
-}
-export async function dbRow(sql, args = []) {
-	const results = await dbQuery(sql, args);
-
-	if (results[0]) return results[0];
-	else return false;
-}
-
-export async function dbUpsert(table, obj, uniqueKeys) {
+export async function dbUpsert(table: any, obj: { [s: string]: unknown; } | ArrayLike<unknown>, uniqueKeys: string | string[]) {
 	const sql = `insert into ${table} (${Object.keys(obj).join(",")})
   values (${Object.values(obj)
-		.map((v) => `'${v}'`)
-		.join(",")}) 
+			.map((v) => `'${v}'`)
+			.join(",")}) 
   on duplicate key update ${Object.keys(obj)
-		.filter((k) => uniqueKeys.indexOf(k) < 0)
-		.map((k) => {
-			return ` ${k}='${obj[k]}' `;
-		})
-		.join(",")}`;
-	try {
+			.filter((k) => uniqueKeys.indexOf(k) < 0)
+			.map((k) => {
+				return ` ${k}='${obj[k]}' `;
+			})
+			.join(",")}`;
+	try
+	{
 		const { insertId } = await dbQuery(sql);
 		return insertId;
-	} catch (e) {
+	} catch (e)
+	{
 		console.error(e);
 	}
 }
 
-export async function dbInsert(table, obj) {
+export async function dbInsert(table: string, obj: { [s: string]: unknown; } | ArrayLike<unknown>) {
 	const sql = `insert into ${table} (${Object.keys(obj).join(",")})
   values (${Object.values(obj)
-		.map((v) => `'${v}'`)
-		.join(",")})`;
+			.map((v) => `'${v}'`)
+			.join(",")})`;
 	const result = await dbQuery(sql).catch((err) => console.error(err));
 	console.log(result);
 	return result;
@@ -82,8 +69,8 @@ export const genUserName = () => {
 		.readSync("usernames.txt")
 		.toString()
 		.split("\n")
-		.map((n) => n.trim())
-		.map((name) =>
+		.map((n: string) => n.trim())
+		.map((name: any) =>
 			require("./src/db").dbInsert("available_usernames", {
 				username: name,
 				taken: 0,
@@ -91,7 +78,7 @@ export const genUserName = () => {
 		);
 };
 
-export async function getOrCreateUser(username) {
+export async function getOrCreateUser(username: string) {
 	return {};
 	// let user = await dbRow("SELECT * from user where username = ? limit 1", [
 	// 	username,
@@ -110,17 +97,17 @@ export async function getOrCreateUser(username) {
 	// return user;
 }
 
-export const hashCheckAuthLogin = async (username) => {
+export const hashCheckAuthLogin = async (username: any) => {
 	return require("exec").exec(
 		`md5 -s '${username}${process.env.secret_md5_salt || "welcome"}'`,
-		(err, stdout, stderr) => {
+		(err: any, stdout: { toString: () => any; }, stderr: any) => {
 			if (err) throw err;
 			return stdout.toString();
 		}
 	);
 };
 
-export const userFiles = async (user) =>
+export const userFiles = async (user: { id: any; }) =>
 	await dbQuery(
 		`select f.*, m.meta as meta 
 from user u 
@@ -131,7 +118,7 @@ from user u
 	).catch((e) => {
 		console.error(e);
 	});
-export const insertVid = (id, title, description) => {
+export const insertVid = (id: any, title: any, description: any) => {
 	dbInsert("ytvid", {
 		title,
 		id,
@@ -141,15 +128,15 @@ export const insertVid = (id, title, description) => {
 		.catch(console.error);
 };
 
-export const insertYtResp = (items) => {
-	items.forEach((item) =>
+export const insertYtResp = (items: any[]) => {
+	items.forEach((item: { id: { videoId: any; }; snippet: { title: any; description: any; }; }) =>
 		insertVid(item.id.videoId, item.snippet.title, item.snippet.description)
 	);
 };
 
-export function queryYtWithDb(query, cb) {}
+export function queryYtWithDb(query: any, cb: any) { }
 
-export const queryYt = (query, count, cb?) => {
+export const queryYt = (query: string, count: any, cb?: (arg0: any) => any) => {
 	const youtube_api_key = process.env.google_key;
 	const url =
 		`https://www.googleapis.com/youtube/v3/search?type=video` +
